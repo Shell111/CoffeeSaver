61db916613f223f4f4d2e8786b6e206307a1d2da
Shell <shellfarley@gmail.com>
2021-09-22 14:19:46 +1000

    surge deployment script added, about and input amount components changed and about styles added

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
-
+import './About.css'
 
 export function About() {
 
   return(
     <div className="about">
-      <h2>About the Coffee Saver App</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus similique in reprehenderit vero veniam? Cumque et optio, accusamus nesciunt officia enim dolorum rerum ex minus autem fugit repudiandae blanditiis iusto.</p>
-
+      <h2 className="intro__section--heading  page__text">About Coffee Saver</h2>
+      <div>
+        <p className="about__desc page__text">This app idea came from the realisation a few years ago that I spent close to $3k on purchased coffees over the year (like oh my gosh... three thousand dollars.. that could have been a holiday!) And while I do absolutely love and enjoy delicious, well made coffees from my local barista, I also recognise I don't need to buy every coffee I drink! So this app is a way to keep track of the coffees I decide not to buy, and then maybe save for that holiday instead! </p>
+      </div>
+      <div>
+        <p className="about__desc page__text">The app can be used for any item you regularly spend money on (doesn't have to be coffee!), just enter the price and quantity, and the total along with the date will be recorded in your browser's local storage. If you'd like to reset the current total, simply click Clear Balance and your saved history will be cleared and the app reset back to a zero balance.</p>
+      </div>
     </div>
 
   )
--- src/components/InputAmount.js
+++ src/components/InputAmount.js
@@ -11,8 +11,8 @@ export function InputAmount({amountInput, onChange}){
         <input value={amountInput} 
           onChange={onChange} 
           className="input__section--field" 
-          type="number" 
-          placeholder="Enter amount here"
+          type="number" step="0.01" placeholder="0.00" 
+          // placeholder="Enter amount here"
         />
           {/* <p>${amountInput}</p> */}
       </div>
